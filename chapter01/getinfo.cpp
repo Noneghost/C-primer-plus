b3cde2b67d62429f0e6d2b43e4effcfbbefacd4d
MaesMustang <zcPython_crawler@yeah.net>
2018-07-24 22:45:29 +0800

    Create getinfo.cpp

---
+++ chapter01/getinfo.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main()
+{
+  using namespace std;
+  
+  int carrots;
+  
+  cout << "How many carrots do you have?" <<endl;
+  cin << carrots;
+  cout << "Here are two more.";
+  carrtos = carrtos + 2 ; 
+  cout << "Now you have "<< carrtos << "carrtos." << endl;
+  
+    
+  return 0;
+}
