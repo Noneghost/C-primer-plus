df3834d6163cc9e0525badc5ff0c70d5a35033c0
MaesMustang <zcPython_crawler@yeah.net>
2018-07-24 22:50:29 +0800

    Update getinfo.cpp

--- chapter01/getinfo.cpp
+++ chapter01/getinfo.cpp
@@ -7,10 +7,10 @@ int main()
   int carrots;
   
   cout << "How many carrots do you have?" <<endl;
-  cin << carrots;
+  cin >> carrots;
   cout << "Here are two more.";
-  carrtos = carrtos + 2 ; 
-  cout << "Now you have "<< carrtos << "carrtos." << endl;
+  carrots = carrots + 2 ; 
+  cout << "Now you have "<< carrots << " carrots." << endl;
   
     
   return 0;
