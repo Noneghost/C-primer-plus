1fcc70f339d098153fe16798c3ad1e0babd75823
MaesMustang <zcPython_crawler@yeah.net>
2018-07-27 22:25:26 +0800

    Create limits.cpp

---
+++ chapter_02/limits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <climits>
+
+int main(void)
+{ 
+  using namespace std;
+  
+  int n_int = INT_MAX;
+  short n_short = SHRT_MAX;
+  long n_long = LONG_MAX;
+  long long n_llong = LLONG_MAX;
+  
+  cout <<"int is "<< sizeof(n_int)<<" bytes"<<endl;
+  cout <<"short is "<< sizeof(n_short)<<" bytes"<<endl;
+  cout <<"long is "<< sizeof(n_long)<<" bytes"<<endl;
+  cout <<"long long  is "<< sizeof(n_llong)<<" bytes"<<endl;
+  return 0;
+}
